94c4e23067096b25f66c892bb7076fad8d7573c1
t3kpunk <widmar@hotmail.com>
2019-04-21 09:30:10 +0200

    modified:   config-org.cfg 	new file:   mqtt_log_omnik.py 	new file:   outputs/MQTTOutput.py 	new file:   outputs/MQTTOutput.txt

---
+++ mqtt_log_omnik.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""
+Get data from the omniksol inverter and output to console. This is a small
+utility program that just changes the config to output anything to mqtt.
+"""
+import OmnikExport
+import time
+from time import sleep
+
+
+if __name__ == "__main__":
+    omnik_exporter = OmnikExport.OmnikExport('config.cfg')
+
+    omnik_exporter.override_config('general', 'enabled_plugins',
+                                   'MQTTOutput')
+    omnik_exporter.override_config('log', 'type', 'console')
+    omnik_exporter.override_config('log', 'level', 'error') #'debug'
+
+    # run every 30 seconds starting on hh:mm:00 or hh:mm:30
+    while True:
+        while (time.time() % 30 < 0.001):
+            omnik_exporter.run()
+            sleep(0.001)
+
